e121e180f2a750de9cbfac2ff01328d8cf910672
dolodorsey <dolodorsey@gmail.com>
2026-06-23 23:28:32 -0400

    Add department operating system page

---
+++ src/app/ops-os/departments/page.tsx
@@ -0,0 +1,7 @@
+import { DepartmentCommand } from "../DepartmentCommand";
+
+export const metadata = { title: "Department OS | KHG Ops OS" };
+
+export default function DepartmentsPage() {
+  return <DepartmentCommand divisionKey="departments" />;
+}
