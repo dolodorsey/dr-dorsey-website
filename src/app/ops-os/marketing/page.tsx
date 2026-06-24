4d067995b33c243f61fa2d74d6f103189e58d28a
dolodorsey <dolodorsey@gmail.com>
2026-06-24 01:13:05 -0400

    Route marketing command to premium dashboard

---
+++ src/app/ops-os/marketing/page.tsx
@@ -0,0 +1,7 @@
+import MarketingCommandPro from "./MarketingCommandPro";
+
+export const metadata = { title: "Marketing Command | KHG Ops OS" };
+
+export default function MarketingPage() {
+  return <MarketingCommandPro />;
+}
