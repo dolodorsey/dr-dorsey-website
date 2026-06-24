40a488a047c84d150f6569da683a1945db6ad9a1
dolodorsey <dolodorsey@gmail.com>
2026-06-23 23:28:51 -0400

    Add products ops dashboard page

---
+++ src/app/ops-os/products/page.tsx
@@ -0,0 +1,7 @@
+import { DepartmentCommand } from "../DepartmentCommand";
+
+export const metadata = { title: "Products Division | KHG Ops OS" };
+
+export default function ProductsPage() {
+  return <DepartmentCommand divisionKey="products" />;
+}
