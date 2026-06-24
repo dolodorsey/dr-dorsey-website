4cefd1a5eb59e49ce7cc64f82dd4794728e85eb9
dolodorsey <dolodorsey@gmail.com>
2026-06-24 01:36:40 -0400

    Route tasks through dynamic Ops section

--- src/app/ops-os/tasks/page.tsx
+++
@@ -1,7 +0,0 @@
-import Workspace from "../Workspace";
-
-export const metadata = { title: "Task Command | KHG Ops OS" };
-
-export default function TasksPage() {
-  return <Workspace section="tasks" />;
-}
