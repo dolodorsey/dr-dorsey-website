757b4c6f20c3c79fd6e160f69e33b0d733ea8954
dolodorsey <dolodorsey@gmail.com>
2026-06-23 23:31:07 -0400

    Show department dashboard ribbon in Ops OS

--- src/app/ops-os/layout.tsx
+++ src/app/ops-os/layout.tsx
@@ -1,6 +1,7 @@
+import DepartmentRibbon from "./DepartmentRibbon";
 import OpsShell from "./OpsShell";
 import "./ops.css";
 
 export default function OpsLayout({ children }: { children: React.ReactNode }) {
-  return <OpsShell>{children}</OpsShell>;
+  return <OpsShell><DepartmentRibbon />{children}</OpsShell>;
 }
