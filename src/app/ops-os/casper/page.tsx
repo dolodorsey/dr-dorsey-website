ea64bcca1fa7a6ee34c1e361f246ebbf9381485f
dolodorsey <dolodorsey@gmail.com>
2026-06-23 23:28:39 -0400

    Add Casper Group ops dashboard page

---
+++ src/app/ops-os/casper/page.tsx
@@ -0,0 +1,7 @@
+import { DepartmentCommand } from "../DepartmentCommand";
+
+export const metadata = { title: "Casper Group | KHG Ops OS" };
+
+export default function CasperPage() {
+  return <DepartmentCommand divisionKey="casper" />;
+}
