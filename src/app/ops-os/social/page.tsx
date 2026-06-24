2fe7fc5e0e8d152cfea0d309313de552dc726bfa
dolodorsey <dolodorsey@gmail.com>
2026-06-24 01:12:14 -0400

    Route social command to premium dashboard

---
+++ src/app/ops-os/social/page.tsx
@@ -0,0 +1,7 @@
+import SocialCommandPro from "./SocialCommandPro";
+
+export const metadata = { title: "Social Media Command | KHG Ops OS" };
+
+export default function SocialPage() {
+  return <SocialCommandPro />;
+}
