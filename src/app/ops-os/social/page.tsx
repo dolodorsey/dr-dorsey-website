c99db4915af0eaef6b9f464970392aa17b6333b6
dolodorsey <dolodorsey@gmail.com>
2026-06-24 01:35:25 -0400

    Route social through dynamic Ops section

--- src/app/ops-os/social/page.tsx
+++
@@ -1,7 +0,0 @@
-import SocialCommandPro from "./SocialCommandPro";
-
-export const metadata = { title: "Social Media Command | KHG Ops OS" };
-
-export default function SocialPage() {
-  return <SocialCommandPro />;
-}
