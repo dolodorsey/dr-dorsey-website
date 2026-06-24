c8d210fe01cf2fe14c3d1a2de7e06d1f66d8160a
dolodorsey <dolodorsey@gmail.com>
2026-06-24 01:35:46 -0400

    Route marketing through dynamic Ops section

--- src/app/ops-os/marketing/page.tsx
+++
@@ -1,7 +0,0 @@
-import MarketingCommandPro from "./MarketingCommandPro";
-
-export const metadata = { title: "Marketing Command | KHG Ops OS" };
-
-export default function MarketingPage() {
-  return <MarketingCommandPro />;
-}
