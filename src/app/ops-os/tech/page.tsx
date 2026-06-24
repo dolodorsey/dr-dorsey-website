11e9fb6572540facd80c918202eec973cd0f67cf
dolodorsey <dolodorsey@gmail.com>
2026-06-23 23:29:02 -0400

    Add tech ops dashboard page

---
+++ src/app/ops-os/tech/page.tsx
@@ -0,0 +1,7 @@
+import { DepartmentCommand } from "../DepartmentCommand";
+
+export const metadata = { title: "Tech Division | KHG Ops OS" };
+
+export default function TechPage() {
+  return <DepartmentCommand divisionKey="apps-tech" />;
+}
