9911e7c6d3f5008b702d818c862b50c32400f138
dolodorsey <dolodorsey@gmail.com>
2026-08-03 18:41:41 -0400

    Emergency rollback unstable mobile enhancer

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,13 +1,11 @@
 import CustomerAppEnhancer from "./customer-app-enhancer";
 import CustomerAppGateway from "./customer-app-gateway";
 import CustomerAppV2 from "./customer-app-v2";
-import MobileExperienceEnhancer from "./mobile-experience-enhancer";
 
 export default function KollectiveCustomerAppPage() {
   return (
     <CustomerAppGateway>
       <CustomerAppEnhancer />
-      <MobileExperienceEnhancer />
       <CustomerAppV2 />
     </CustomerAppGateway>
   );
