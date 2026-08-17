40b9152d407f2fa2e4e3f860b7c6cf4f4562829e
dolodorsey <dolodorsey@gmail.com>
2026-08-17 15:52:06 -0400

    Assemble generated team portrait sprite

---
+++ src/data/team-portraits/index.ts
@@ -0,0 +1,7 @@
+import chunk01 from './chunk01';
+import chunk02 from './chunk02';
+import chunk03 from './chunk03';
+import chunk04 from './chunk04';
+import chunk05 from './chunk05';
+
+export const TEAM_PORTRAIT_SPRITE_BASE64 = `${chunk01}${chunk02}${chunk03}${chunk04}${chunk05}`;
